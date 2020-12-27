519d9a0305028b2be07c210f71318a774eadb29e
Charles Jo <charles68jo@gmail.com>
2020-12-27 19:28:58 +0900

    TEST RESULT

--- main.cpp
+++ main.cpp
@@ -7,5 +7,16 @@ int main() {
     ApTest test;
     test.fs1(1);
     test.fs2(1); 
+        test.fs1(1);
+    test.fs2(1); 
+        test.fs1(1);
+    test.fs2(1); 
+        test.fs1(1);
+    test.fs2(1); 
+        test.fs1(1);
+    test.fs2(1); 
+        test.fs1(1);
+    test.fs2(1); 
+    
     return 0;
 }
