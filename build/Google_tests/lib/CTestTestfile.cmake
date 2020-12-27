02a2abdff8df6040ebebf10372768198ab591e8d
Charles Jo <charles68jo@gmail.com>
2020-12-27 17:43:08 +0900

    test

--- build/Google_tests/lib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charlesjo/ap/jenkinsTestApp/Google_tests/lib
-# Build directory: /home/charlesjo/ap/jenkinsTestApp/build/Google_tests/lib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(googlemock)
